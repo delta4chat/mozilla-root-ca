365beae4022835f66515f8e082d474f616c03e26
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2024-12-17 15:18:49 +0800

    bump-version.sh: avoid mistake if more than one "^version =" exists

--- bump-version.sh
+++ bump-version.sh
@@ -5,12 +5,12 @@ set -x
 
 d="$(date -u '+%Y.%m.%d')"
 
-n="$(cat Cargo.toml | grep -n '^version = "' | tr ':' ' ' | awk '{print $1}')"
+n="$(cat Cargo.toml | grep -n -F '###BUMP-VERSION.SH###' | tr ':' ' ' | awk '{print $1}')"
 l="$(cat Cargo.toml | wc -l)"
 
 {
     cat Cargo.toml | head -n $[n-1]
-    echo "version = \"${d}\""
+    echo "version = \"${d}\" ###BUMP-VERSION.SH###"
     cat Cargo.toml | tail -n $[l-n]
 } > .Cargo.toml.new
 
