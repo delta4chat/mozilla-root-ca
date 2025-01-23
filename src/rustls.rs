2c27950b47f704afe85b677f783a0ffc60b2a1a5
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2025-01-23 22:19:53 +0800

    add rustls-trust-anchor feature

--- src/rustls.rs
+++ src/rustls.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-use rustls_pki_types::CertificateDer;
+use rustls_pki_types::{CertificateDer, Der};
 
 pub const RUSTLS_CERTIFICATE_DER_LIST: [CertificateDer<'static>; DER_LIST_LEN] = {
     let mut list = [const { CertificateDer::from_slice(b"") }; DER_LIST_LEN];
@@ -13,4 +13,49 @@ pub const RUSTLS_CERTIFICATE_DER_LIST: [CertificateDer<'static>; DER_LIST_LEN] =
 
     list
 };
+
+pub const RUSTLS_DER_LIST: [Der<'static>; DER_LIST_LEN] = {
+    let mut list = [const { Der::from_slice(b"") }; DER_LIST_LEN];
+
+    let mut i = 0;
+    while i < DER_LIST_LEN {
+        list[i] = Der::from_slice(DER_LIST[i]);
+        i += 1;
+    }
+
+    list
+};
+
+#[cfg(feature="rustls-trust-anchor")]
+use rustls_pki_types::TrustAnchor;
+
+#[cfg(feature="rustls-trust-anchor")]
+pub fn rustls_trust_anchor_list() -> &'static [TrustAnchor<'static>; DER_LIST_LEN] {
+    #[cfg(not(feature="std"))]
+    {
+        use lazy_static::lazy_static;
+
+        lazy_static! {
+            static ref LIST: [TrustAnchor<'static>; DER_LIST_LEN] = gen_anchor_list();
+        }
+
+        &*LIST
+    }
+
+    #[cfg(feature="std")]
+    {
+        use once_cell::sync::Lazy;
+
+        static LIST: Lazy<[TrustAnchor<'static>; DER_LIST_LEN]> = Lazy::new(gen_anchor_list);
+
+        &*LIST
+    }
+}
+
+#[cfg(feature="rustls-trust-anchor")]
+fn gen_anchor_list() -> [TrustAnchor<'static>; DER_LIST_LEN] {
+    core::array::from_fn(|i| {
+        webpki::anchor_from_trusted_cert(&RUSTLS_CERTIFICATE_DER_LIST[i]).unwrap()
+    })
+}
 
